48ab67199a6e343bc76a36bd11f6a36623585908
Tom Selley <42682539+makinprinter@users.noreply.github.com>
2025-07-08 17:04:45 -0400

    Update FullReportMaker.py

--- FullReportMaker.py
+++ FullReportMaker.py
@@ -11,7 +11,6 @@ def SortData(test_x,test_y,ListForSort,attribute_dict):
     Sorted_data = {}
     Sorted_List = [] 
     for i, image in tqdm(enumerate(test_x)):
-        
         O, I, H_l = Is_O_or_I_old(image,ListForSort[0],ListForSort[1])
         key = f"O_{O}_I_{I}_Hl_{H_l}"
         Flag = True 
@@ -19,8 +18,7 @@ def SortData(test_x,test_y,ListForSort,attribute_dict):
             if keyTest == key:
                 Flag = False
         if Flag:
-            key = "ALL"
-            
+            key = "ALL" 
         if key not in Sorted_List:
             Sorted_data[key]  = {"data":[],"indexs":[]}
             Sorted_List.append(key)
@@ -28,20 +26,12 @@ def SortData(test_x,test_y,ListForSort,attribute_dict):
         Sorted_data[key]["indexs"].append(i)
         
     return Sorted_data
-            
-        
-    
-    
 
 def CombinedReportMaker(ListForSort,attribute_dict,test_x, test_y):
-    
-    
     with open("models/model_index.pkl", "rb") as f:
         model_paths = pickle.load(f)
     for i, image in enumerate(test_x):
-       
         if i == 1:
             break
-    
     return 0;
             
