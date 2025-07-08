6ec7e8c1dab2c17aee1dd8269de45c884fc548b5
Tom Selley <42682539+makinprinter@users.noreply.github.com>
2025-07-08 17:05:14 -0400

    Update Opener.py

--- Opener.py
+++ Opener.py
@@ -1,5 +1,3 @@
-# You will need to run the below code.
-# pip install tensorflow-datasets
 import tensorflow_datasets as tfds
 import numpy as np
 from keras.utils import to_categorical
@@ -24,19 +22,13 @@ def prepare_data(ds):
 train_x, train_y = prepare_data(ds_train)
 test_x, test_y = prepare_data(ds_test)
 
-
 train_x = train_x.astype('float32') / 255.0
 test_x = test_x.astype('float32') / 255.0
-
 train_x = train_x.reshape(-1, 28, 28, 1)
 test_x = test_x.reshape(-1, 28, 28, 1)
-
-# One-hot encode labels
 num_classes = ds_info.features['label'].num_classes
 train_y = to_categorical(train_y, num_classes)
 test_y = to_categorical(test_y, num_classes)
-
-# Optionally merge datasets
 Full_x = np.concatenate((train_x, test_x), axis=0)
 Full_y = np.concatenate((train_y, test_y), axis=0)
 
